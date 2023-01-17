50ef03e94f04be3512f87bc4f35a0cb7fcbdc33a
Jaime Feldman <jaime.feldman@mac.com>
2023-01-17 16:19:21 -0300

    Agregando parametros para init
    Se agregaron:
    -n --appname: que especifica el nombre del la aplicación,
    -l --licence: licencia con 7 opciones por el momento,
    -a --author:  nombre del author del proyecto. el cual acepta n numero de palabras.

--- launcher/__main__.py
+++ launcher/__main__.py
@@ -5,13 +5,39 @@ def main():
 
     frase = frases()
 
-    parser = argparse.ArgumentParser(prog='pypa', description='Generator de paqutes python, para linea de comandos.', epilog=frase.una_frase())
+    parser = argparse.ArgumentParser(prog='pypa', 
+                                     description='Generator de paqutes python, para linea de comandos.', 
+                                     epilog=frase.una_frase())
+
     subparser = parser.add_subparsers(help='opciones iniciales.')
 
     # creando las opciones para el comando init
-    init_parser = subparser.add_parser('init', help='Inicializa una estrucutra de proyecto')
-    init_parser.add_argument('nombre', help='El nombre del proyecto o directorio contenedor.')
-    init_parser.add_argument('-t', '--type', help='el tipo de proyecto a generar, [exe | lib]', required=True, choices=['exe','lib'])
+    init_parser = subparser.add_parser('init', help='Inicializa una estrucutra de proyecto.')
+    init_parser.add_argument('project_name', help='El nombre del proyecto o directorio contenedor.')
+    init_parser.add_argument('-t', 
+                             '--type', 
+                             help='el tipo de proyecto a generar, [exe | lib]', 
+                             required=True, 
+                             choices=['exe','lib'])
+
+    init_parser.add_argument('-n', 
+                             '--appname', 
+                             help='El nombre del la aplicación.', 
+                             required=True, 
+                             type=str)    
+    
+    init_parser.add_argument('-l',
+                             '--licence', 
+                             help='Especifíca la licencia para el proyecto.',
+                             required=True,
+                             choices=['bsd', 'mit', 'lgpl', 'gpl2', 'gpl3', 'mpl'])
+    
+    init_parser.add_argument('-a', 
+                             '--author',
+                             nargs='+',
+                             help='El nombre del autor del la app.',
+                             required=True,
+                             type=str)
 
     # si no hay argumentos muestre la ayuda
     if len(sys.argv) == 1:
